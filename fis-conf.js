899023e80d684cb34825956c3dbf53addbf45278
xukai <karsa@foxmail.com>
2015-01-12 16:48:21 +0800

    edit

--- fis-conf.js
+++ fis-conf.js
@@ -1,15 +1,13 @@
+//设置需要自动打包的文件
 fis.config.set('pack', {
-    'app_js/lib.js': [
-        '/app_js/**.js',
-        '/app_css/backbone/**.js',
-        '/app_img/jquery/**.js',
-        '/modules/vendor/**.js',
-        '/modules/common/**.js'
+    'app_js/im/v1/lib.js': [
+        '/app_js/im/v1/**.js'
+    ],
+    'app_css/im/v1/lib.css': [
+        '/app_css/im/v1/**.less'
     ]
 });
 
-//静态资源域名，使用pure release命令时，添加--domains或-D参数即可生效
-//fis.config.set('roadmap.domain', 'http://www.pingan.com');
 
 fis.config.merge({
     roadmap : {
@@ -24,9 +22,9 @@ fis.config.merge({
         domain : {
             //所有css文件添加http://localhost:8080作为域名
             '**.css' : 'http://css2.pingan.com',
-            '**.less' : 'http://css2.pingan.com',
-            '**.scss' : 'http://css2.pingan.com',
-            '**.js' : 'http://script2.pingan.com',
+            '**.less': 'http://css2.pingan.com',
+            '**.scss': 'http://css2.pingan.com',
+            '**.js'  : 'http://script2.pingan.com',
             '**.jpg' : 'http://img2.pingan.com',
             '**.gif' : 'http://img2.pingan.com',
             '**.png' : 'http://img2.pingan.com'
@@ -63,11 +61,11 @@ fis.config.merge({
 //如果要兼容低版本ie显示透明png图片，请使用pngquant作为图片压缩器，
 //否则png图片透明部分在ie下会显示灰色背景
 //使用spmx release命令时，添加--optimize或-o参数即可生效
-//fis.config.set('settings.optimzier.png-compressor.type', 'pngquant');
+fis.config.set('settings.optimzier.png-compressor.type', 'pngquant');
 
 //设置jshint插件要排除检查的文件，默认不检查lib、jquery、backbone、underscore等文件
 //使用pure release命令时，添加--lint或-l参数即可生效
-fis.config.set('settings.lint.jshint.ignored', [ 'lib/**', /jquery|backbone|underscore/i ]);
+fis.config.set('settings.lint.jshint.ignored', [ 'g/**', /jquery|backbone|underscore/i ]);
 
 //开始autoCombine可以将零散资源进行自动打包
 fis.config.set('settings.postpackager.simple.autoCombine', true);
@@ -109,26 +107,12 @@ fis.config.set('settings.spriter.csssprites', {
     layout: 'matrix'
 });
 
-//如果想配置html中的css片段进行csssprites处理，可以像下面这样配置
-fis.config.set('settings.spriter.csssprites', {
-    //开启模板内联css处理,默认关闭
-    htmlUseSprite: true,
-    //默认针对html原生<style></style>标签内的内容处理。
-    //用户可以通过配置styleTag来扩展要识别的css片段
-    //以下是默认<style></style>标签的匹配正则
-    styleReg: /(<style(?:(?=\s)[\s\S]*?["'\s\w\/\-]>|>))([\s\S]*?)(<\/style\s*>|$)/ig
-
-    //**styleReg规则**
-    //1. 默认不配置styleReg，仅支持html中默认style标签中的css内容
-    //2. 配置styleReg时候，仅支持styleReg匹配到的内容。
-    //3. styleReg正则必须捕获三个分组，
-    //     $1为：开始标签（start tag），
-    //     $2为：内容(content) ,
-    //     $3为：结束标签(end tag)
-});
 
 //项目排除掉_xxx.scss，这些属于框架文件，不用关心
-//fis.config.set('project.exclude', '**/_*.scss');
+fis.config.set('project.exclude', '**/_*.scss');
+fis.config.set('project.exclude', '**/_*.less');
+
+
 //fis.config.set('project.exclude', '**/g/*.*');
 //scss后缀的文件，用fis-parser-sass插件编译
 //fis.config.set('modules.parser.scss', 'sass');
